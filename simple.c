ea3ae6740e242ea6d9982f93219fed50beb57cc5
DEEKSHITHADS2007 <deekshithads2007@gmail.com>
2025-05-13 16:11:31 +0530

    Create simple.c

---
+++ simple.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+{
+ int sum,diffrence,product,quotient;
+ int a,b;
+ printf("enter the value of a and b \n");
+ scanf("%d%d",&a,&b);
+ sum=a+b;
+ diffrence=a-b;
+ product=a*b;
+ quotient=a/b;
+ printf("sum=%d\n",sum);
+ printf("diffrence=%d\n",diffrence);
+ printf("product=%d\n",product);
+ printf("quotient=%d\n",quotient);
+}
