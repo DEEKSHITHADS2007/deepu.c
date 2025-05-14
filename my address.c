b971e69472dd9e6881b6f75220c888d6b5b4c8b5
DEEKSHITHADS2007 <deekshithads2007@gmail.com>
2025-05-14 10:07:37 +0530

    Create my address.c

---
+++ my address.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int usn,age,weight,height;
+char name, college_name;
+printf("enter your name");
+scanf("%s",&name);
+printf("enter your age");
+scanf("%d",&age);
+printf("enter your usn");
+scanf("%d",&usn);
+printf("enter your college_name");
+scanf("%s",&college_name);
+printf("enter your weight");
+scanf("%d",&weight);
+printf("enter your height");
+scanf("%d",&height);
+}
