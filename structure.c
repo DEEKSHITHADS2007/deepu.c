1033f6f48f3ba74b85766b87eb91de28505f9911
DEEKSHITHADS2007 <deekshithads2007@gmail.com>
2025-05-16 14:58:04 +0530

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    struct deepu {
+        char name[50];
+        int age;
+        float weight;
+    };
+    struct deepu d1;
+    printf("enter the name");
+    scanf("%s",&d1.name);
+    printf("enter the age");
+    scanf("%d",&d1.age);
+    printf("enter the weight");
+    scanf("%s",&d1.weight);
+}
+    
